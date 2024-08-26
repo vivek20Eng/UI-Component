67cce7d571c92df64113dd6ef505e1c6f4a4aa33
Vivek <vivek0sv@gmail.com>
2024-08-26 18:04:19 +0530

    ui changes

--- app/components/PageLayout/PageLayout.js
+++ app/components/PageLayout/PageLayout.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PageLayout = ({ title, children }) => {
   return (
-    <div className="container py-8 m-2">
-      <h1 className="text-3xl font-bold sm:mb-6 sm:text-left">{title}</h1>
+    <div className="container py-0 sm:py-8 m-2">
+      <h1 className="text-3xl font-bold sm:mb-6 sm:text-left mb-6">{title}</h1>
       {children}
     </div>
   );
